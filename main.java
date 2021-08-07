70eb194d9cdf0ab9406b5045f44baac843081f27
Shivam Tayal <shivamtayal@icloud.com>
2021-08-07 15:04:17 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,27 @@
+package tictactoe;
+import java.util.*;
 
+public class Main {
+    
+    static void printRes(String cells) {
+        char[] cellsArr = cells.toCharArray();
+        System.out.println("---------");
+        int k = 0;
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(cellsArr[k] + " ");
+                k++;
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
+    }
+    
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+        String cells = cin.next();
+        printRes(cells);
+    }
+}
